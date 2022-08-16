0f1dcc54a7057a7f09f0db9ad812e9d45d6f606a
NoahDavon <37221199+NoahDavon@users.noreply.github.com>
2022-08-16 22:35:31 +0200

    Update firebase.js

--- zen_website/lib/firebase.js
+++ zen_website/lib/firebase.js
@@ -5,19 +5,7 @@ import 'firebase/compat/firestore';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDlq85Uk6nH994MRD1AFPNkJQbzU56KgQM",
 
-    authDomain: "zenspecialtycoffee-d42a6.firebaseapp.com",
-  
-    projectId: "zenspecialtycoffee-d42a6",
-  
-    storageBucket: "zenspecialtycoffee-d42a6.appspot.com",
-  
-    messagingSenderId: "546719735274",
-  
-    appId: "1:546719735274:web:5c300ed919dabd5d352693",
-  
-    measurementId: "G-HGTNXR19BQ"  
   };
   
 if (!firebase.apps.length) {
